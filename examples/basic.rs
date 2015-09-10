1b5bbdbb50e8a39280c28911bb73870bb68faf7b
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-09-10 16:38:16 +0200

    Use a BufReader in the examples

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,15 +1,17 @@
 extern crate rodio;
 
+use std::io::BufReader;
+
 fn main() {
     let endpoint = rodio::get_default_endpoint().unwrap();
 
     let file = std::fs::File::open("examples/beep.wav").unwrap();
-    let beep1 = rodio::play_once(&endpoint, file);
+    let beep1 = rodio::play_once(&endpoint, BufReader::new(file));
 
     std::thread::sleep_ms(1000);
 
     let file = std::fs::File::open("examples/beep2.wav").unwrap();
-    rodio::play_once(&endpoint, file);
+    rodio::play_once(&endpoint, BufReader::new(file));
 
     std::thread::sleep_ms(1000);
     /*let file = std::fs::File::open("examples/beep3.ogg").unwrap();
--- examples/music_wav.rs
+++ examples/music_wav.rs
@@ -1,10 +1,12 @@
 extern crate rodio;
 
+use std::io::BufReader;
+
 fn main() {
     let endpoint = rodio::get_default_endpoint().unwrap();
 
     let file = std::fs::File::open("examples/music.wav").unwrap();
-    let _music = rodio::play_once(&endpoint, file);
+    let _music = rodio::play_once(&endpoint, BufReader::new(file));
 
     std::thread::sleep_ms(10000);
 }

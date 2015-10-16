ddc5bec3442705d075d638ed8a0875e79b7a41b9
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-10-16 16:37:48 +0200

    Update the examples to use the new API

--- examples/music_ogg.rs
+++ examples/music_ogg.rs
@@ -4,9 +4,10 @@ use std::io::BufReader;
 
 fn main() {
     let endpoint = rodio::get_default_endpoint().unwrap();
+    let sink = rodio::Sink::new(&endpoint);
 
     let file = std::fs::File::open("examples/music.ogg").unwrap();
-    let _music = rodio::play_once(&endpoint, BufReader::new(file));
+    sink.append(rodio::Decoder::new(BufReader::new(file)));
 
     std::thread::sleep_ms(60000);
 }

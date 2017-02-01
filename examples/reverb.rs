14cdd05bae8adc495f8895264ef15d18d9d850a6
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-02-01 11:05:24 +0100

    Add a reverb example

---
+++ examples/reverb.rs
@@ -0,0 +1,17 @@
+extern crate rodio;
+
+use rodio::Source;
+use std::io::BufReader;
+use std::time::Duration;
+
+fn main() {
+    let endpoint = rodio::get_default_endpoint().unwrap();
+    let sink = rodio::Sink::new(&endpoint);
+
+    let file = std::fs::File::open("examples/music.ogg").unwrap();
+    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+    let with_reverb = source.buffered().reverb(Duration::from_millis(40), 0.7);
+    sink.append(with_reverb);
+
+    sink.sleep_until_end();
+}

a29ad630f2e4ab5f85f4ff283689bcd9258d80ce
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-02-07 12:33:18 +0100

    Expand basic example with printlns

--- examples/basic.rs
+++ examples/basic.rs
@@ -10,21 +10,26 @@ fn main() {
     let file = std::fs::File::open("examples/beep.wav").unwrap();
     let mut beep1 = rodio::play_once(&endpoint, BufReader::new(file)).unwrap();
     beep1.set_volume(0.2);
+    println!("Started beep1");
 
-    thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1500));
 
     let file = std::fs::File::open("examples/beep2.wav").unwrap();
     rodio::play_once(&endpoint, BufReader::new(file)).unwrap().detach();
+    println!("Started beep2");
 
-    thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1500));
     let file = std::fs::File::open("examples/beep3.ogg").unwrap();
     let beep3 = rodio::play_once(&endpoint, file).unwrap();
+    println!("Started beep3");
 
-    thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1500));
     drop(beep1);
+    println!("Stopped beep1");
 
-    thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1500));
     drop(beep3);
+    println!("Stopped beep3");
 
-    thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1500));
 }

a1e6906e8c62a2c51845d221971aabb4704067ce
AlexNav73 <fifthhorseman@mail.ru>
2018-01-27 17:22:57 +0300

    Fixed mp3 example

--- examples/music_mp3.rs
+++ examples/music_mp3.rs
@@ -3,12 +3,11 @@ extern crate rodio;
 use std::io::BufReader;
 
 fn main() {
-    let endpoint = rodio::get_default_endpoint().unwrap();
+    let endpoint = rodio::default_endpoint().unwrap();
     let sink = rodio::Sink::new(&endpoint);
 
     let file = std::fs::File::open("examples/music.mp3").unwrap();
-    sink.append(rodio::Decoder::new(BufReader::new(file)));
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
-    //sink.sleep_until_end();
-    std::thread::sleep_ms(32000);
+    sink.sleep_until_end();
 }

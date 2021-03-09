99e0c6b0bfcf8ca6cb8893ab384921b519748900
Nathan Stocks <cleancut@github.com>
2021-03-08 23:46:51 -0700

    Improve documentation about getting default output stream to a device (#357)
    * Improve documentation about getting default output stream to a device
    
    * Use intra-doc links. Provide examples that can compile and run.
    
    * Restore no_run
    
    * More no_run

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,77 @@
 //! Audio playback library.
 //!
-//! The main concept of this library is [the `Source` trait](source/trait.Source.html), which
+//! The main concept of this library is the [`Source`] trait, which
 //! represents a sound (streaming or not). In order to play a sound, there are three steps:
 //!
 //! - Create an object that represents the streaming sound. It can be a sine wave, a buffer, a
-//!   [decoder](decoder/index.html), etc. or even your own type that implements
-//!   [the `Source` trait](source/trait.Source.html).
-//! - Choose an output with the [`devices`](fn.devices.html) or
-//!   [`default_output_device`](fn.default_output_device.html) functions.
-//! - Call [`play_raw(output, source)`](fn.play_raw.html).
+//!   [`decoder`], etc. or even your own type that implements the [`Source`] trait.
+//! - Get an output stream handle to a physical device. For example, get a stream to the system's
+//!   default sound device with [`OutputStream::try_default()`]
+//! - Call [`.play_raw(source)`](OutputStreamHandle::play_raw) on the output stream handle.
 //!
-//! The `play_raw` function expects the source to produce `f32`s, which may not be the case. If you
-//! get a compilation error, try calling `.convert_samples()` on the source to fix it.
+//! The [`play_raw`](OutputStreamHandle::play_raw) function expects the source to produce [`f32`]s,
+//! which may not be the case. If you get a compilation error, try calling
+//! [`.convert_samples()`](Source::convert_samples) on the source to fix it.
 //!
 //! For example, here is how you would play an audio file:
 //!
 //! ```no_run
 //! use std::fs::File;
 //! use std::io::BufReader;
-//! use rodio::Source;
+//! use rodio::{Decoder, OutputStream, source::Source};
 //!
-//! let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-//!
+//! // Get a output stream handle to the default physical sound device
+//! let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 //! // Load a sound from a file, using a path relative to Cargo.toml
-//! let file = File::open("sound.ogg").unwrap();
-//! let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+//! let file = BufReader::new(File::open("examples/music.ogg").unwrap());
+//! // Decode that sound file into a source
+//! let source = Decoder::new(file).unwrap();
+//! // Play the sound directly on the device
 //! stream_handle.play_raw(source.convert_samples());
 //!
 //! // The sound plays in a separate audio thread,
 //! // so we need to keep the main thread alive while it's playing.
-//! // Press ctrl + C to stop the process once you're done.
-//! loop {}
+//! std::thread::sleep(std::time::Duration::from_secs(5));
 //! ```
 //!
 //! ## Sink
 //!
 //! In order to make it easier to control the playback, the rodio library also provides a type
-//! named [`Sink`](struct.Sink.html) which represents an audio track.
+//! named [`Sink`] which represents an audio track.
 //!
-//! Instead of playing the sound with [`play_raw`](fn.play_raw.html), you can add it to a
-//! [`Sink`](struct.Sink.html) instead.
+//! Instead of playing the sound with [`play_raw`](OutputStreamHandle::play_raw), you can add it to
+//! a [`Sink`] instead.
+//!
+//! - Get a [`Sink`] to the output stream, and [`.append()`](Sink::append) your sound to it.
 //!
 //! ```no_run
-//! use rodio::Sink;
+//! use std::fs::File;
+//! use std::io::BufReader;
+//! use std::time::Duration;
+//! use rodio::{Decoder, OutputStream, Sink};
+//! use rodio::source::{SineWave, Source};
 //!
-//! let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
-//! let sink = rodio::Sink::try_new(&stream_handle).unwrap();
+//! let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+//! let sink = Sink::try_new(&stream_handle).unwrap();
 //!
 //! // Add a dummy source of the sake of the example.
-//! let source = rodio::source::SineWave::new(440);
+//! let source = SineWave::new(440).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20);
 //! sink.append(source);
+//!
+//! // The sound plays in a separate thread. This call will block the current thread until the sink
+//! // has finished playing all its queued sounds.
+//! sink.sleep_until_end();
 //! ```
 //!
-//! The [`append` method](struct.Sink.html#method.append) will add the sound at the end of the
+//! The [`append`](Sink::append) method will add the sound at the end of the
 //! sink. It will be played when all the previous sounds have been played. If you want multiple
-//! sounds to play simultaneously, you should create multiple [`Sink`](struct.Sink.html)s.
+//! sounds to play simultaneously, you should create multiple [`Sink`]s.
 //!
-//! The [`Sink`](struct.Sink.html) type also provides utilities such as playing/pausing or
-//! controlling the volume.
+//! The [`Sink`] type also provides utilities such as playing/pausing or controlling the volume.
 //!
 //! ## Filters
 //!
-//! [The `Source` trait](source/trait.Source.html) provides various filters, similarly to the
-//! standard `Iterator` trait.
+//! The [`Source`] trait provides various filters, similar to the standard [`Iterator`] trait.
 //!
 //! Example:
 //!
@@ -78,7 +87,7 @@
 //! ## How it works under the hood
 //!
 //! Rodio spawns a background thread that is dedicated to reading from the sources and sending
-//! the output to the device. Whenever you give up ownership of a `Source` in order to play it,
+//! the output to the device. Whenever you give up ownership of a [`Source`] in order to play it,
 //! it is sent to this background thread where it will be read by rodio.
 //!
 //! All the sounds are mixed together by rodio before being sent to the operating system or the

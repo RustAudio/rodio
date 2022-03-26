69a7f416a73aa612b0ad037aac00c4bf65170227
Alejandro Perea <alexpro820@gmail.com>
2022-03-26 20:15:18 +0100

    Move sound to `assets/`; Exclude it from package (#421)
    * Move sound to `assets/`; Exclude it from package
    
    * Exclude tests from package

--- examples/music_ogg.rs
+++ examples/music_ogg.rs
@@ -4,7 +4,7 @@ fn main() {
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
 
-    let file = std::fs::File::open("examples/music.ogg").unwrap();
+    let file = std::fs::File::open("assets/music.ogg").unwrap();
     sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
     sink.sleep_until_end();

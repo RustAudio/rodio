2676cf65a1c6505af3f9b311e27da4c3dc53fe7a
dis-da-moe <moe.uraijah@gmail.com>
2022-11-22 23:42:46 +0300

    compress RL.wav to RL.ogg

--- examples/stereo.rs
+++ examples/stereo.rs
@@ -4,7 +4,8 @@ use std::io::BufReader;
 fn main() {
     let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
     let sink = rodio::Sink::try_new(&handle).unwrap();
-    let file = std::fs::File::open("assets/RL.wav").unwrap();
+
+    let file = std::fs::File::open("assets/RL.ogg").unwrap();
     sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
 
     sink.sleep_until_end();

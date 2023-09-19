74fae4359329c86f46d838a92f4467be58c5251f
kuviman <kuviman@gmail.com>
2023-09-19 17:30:18 +0400

    Calculate ReadSeekSource byte_len lazily

--- src/decoder/read_seek_source.rs
+++ src/decoder/read_seek_source.rs
@@ -1,11 +1,12 @@
 use std::io::{Read, Result, Seek, SeekFrom};
 use std::marker::Sync;
+use std::sync::{Mutex, RwLock};
 
 use symphonia::core::io::MediaSource;
 
 pub struct ReadSeekSource<T: Read + Seek + Send + Sync> {
-    inner: T,
-    byte_len: Option<u64>,
+    inner: Mutex<T>,
+    byte_len: RwLock<Option<Option<u64>>>, // One option is for lazy calculation
 }
 
 // Copied from std Seek::stream_len since its unstable
@@ -25,9 +26,11 @@ fn stream_len(stream: &mut impl Seek) -> std::io::Result<u64> {
 impl<T: Read + Seek + Send + Sync> ReadSeekSource<T> {
     /// Instantiates a new `ReadSeekSource<T>` by taking ownership and wrapping the provided
     /// `Read + Seek`er.
-    pub fn new(mut inner: T) -> Self {
-        let byte_len = stream_len(&mut inner).ok();
-        ReadSeekSource { inner, byte_len }
+    pub fn new(inner: T) -> Self {
+        ReadSeekSource {
+            inner: Mutex::new(inner),
+            byte_len: RwLock::new(None),
+        }
     }
 }
 
@@ -37,18 +40,31 @@ impl<T: Read + Seek + Send + Sync> MediaSource for ReadSeekSource<T> {
     }
 
     fn byte_len(&self) -> Option<u64> {
-        self.byte_len
+        // Check if length was calculated before
+        let byte_len = self.byte_len.read().unwrap();
+        if let Some(cached) = *byte_len {
+            return cached;
+        }
+        std::mem::drop(byte_len); // Release read lock
+
+        let mut inner = self.inner.lock().unwrap();
+        let calculated_stream_len = match stream_len(&mut *inner) {
+            Ok(len) => Some(len),
+            Err(_) => None, // Ignore error, cache failure
+        };
+        *self.byte_len.write().unwrap() = Some(calculated_stream_len);
+        calculated_stream_len
     }
 }
 
 impl<T: Read + Seek + Send + Sync> Read for ReadSeekSource<T> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        self.inner.read(buf)
+        self.inner.get_mut().unwrap().read(buf)
     }
 }
 
 impl<T: Read + Seek + Send + Sync> Seek for ReadSeekSource<T> {
     fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
-        self.inner.seek(pos)
+        self.inner.get_mut().unwrap().seek(pos)
     }
 }

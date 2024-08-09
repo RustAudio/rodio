eef2edaa3deb87e3af932825aa98b69a3c2983ec
Jamie Hardt <jamiehardt@me.com>
2024-08-09 12:40:46 -0700

    Recent synth changes caught up with master
    ...and upstream.

--- src/source/synth.rs
+++ src/source/synth.rs
@@ -2,17 +2,7 @@ use std::f32::consts::TAU;
 use std::time::Duration;
 
 use super::SeekError;
-use crate::{buffer::SamplesBuffer, source::Repeat, Source};
-
-
-/// Express a frequency as a rational number.
-///     .0 Cycles per time quanta
-///     .1 Time quanta per second
-///
-/// Examples:
-///     1000,1      1000 Hz
-///     12345,100   123.45 Hz
-pub struct RationalFrequency(u32, u32);
+use crate::Source;
 
 /// Syntheizer waveform functions. All of the synth waveforms are in the
 /// codomain [-1.0, 1.0].
@@ -27,77 +17,68 @@ pub enum SynthWaveformFunction {
 impl SynthWaveformFunction {
     /// Create a single sample for the given waveform
     #[inline]
-    pub fn render(&self, sample: u32, period: u32) -> f32 {
-        let i_div_p: f32 = sample as f32 / period as f32;
-            
+    pub fn render(&self, i: u64, period: f32) -> f32 {
+        let i_div_p: f32 = i as f32 / period;
+
         match self {
             Self::Sine => (TAU * i_div_p).sin(),
             Self::Triangle => 04.0f32 * (i_div_p - (i_div_p + 0.5f32).floor()).abs() - 1f32,
             Self::Square => {
-                    if i_div_p < 0.5f32 {
-                        1.0f32
-                    } else {
-                        -1.0f32
-                    }
-                },
+                if i_div_p % 1.0f32 < 0.5f32 {
+                    1.0f32
+                } else {
+                    -1.0f32
+                }
+            }
             Self::Sawtooth => 2.0f32 * (i_div_p - (i_div_p + 0.5f32).floor()),
         }
-    }
-
-    /// Create a `SamplesBuffer` containing one period of `self` with the given
-    /// sample rate and frequency.
-    pub fn create_buffer(
-        &self,
-        sample_rate: cpal::SampleRate,
-        frequency: u32,
-    ) -> SamplesBuffer<f32> {
-        let period: usize = (sample_rate.0 / frequency) as usize;
-        let mut samples_vec = vec![0.0f32; period];
-
-        for i in 0..period {
-            samples_vec[i] = self.render(i as u32, period as u32);
-        }
-
-        SamplesBuffer::new(1, sample_rate.0, samples_vec)
     }
 }
 
 /// An infinite source that produces one of a selection of synthesizer
-/// waveforms from a buffered source.
+/// waveforms.
 #[derive(Clone)]
-pub struct BufferedSynthWaveform {
-    input: Repeat<SamplesBuffer<f32>>,
+pub struct SynthWaveform {
+    sample_rate: cpal::SampleRate,
+    period: f32,
+    f: SynthWaveformFunction,
+    i: u64,
 }
 
-impl BufferedSynthWaveform {
+impl SynthWaveform {
     /// Create a new `SynthWaveform` object that generates an endless waveform
     /// `f`.
     #[inline]
     pub fn new(
         sample_rate: cpal::SampleRate,
-        frequency: u32,
+        frequency: f32,
         f: SynthWaveformFunction,
-    ) -> BufferedSynthWaveform {
-        let buffer = f.create_buffer(sample_rate, frequency);
-        BufferedSynthWaveform {
-            input: buffer.repeat_infinite(),
+    ) -> SynthWaveform {
+        let period = sample_rate.0 as f32 / frequency;
+        SynthWaveform {
+            sample_rate,
+            period,
+            f,
+            i: 0,
         }
     }
 }
 
-impl Iterator for BufferedSynthWaveform {
+impl Iterator for SynthWaveform {
     type Item = f32;
 
     #[inline]
     fn next(&mut self) -> Option<f32> {
-        self.input.next()
+        let this_i = self.i;
+        self.i += 1;
+        Some(self.f.render(this_i, self.period))
     }
 }
 
-impl Source for BufferedSynthWaveform {
+impl Source for SynthWaveform {
     #[inline]
     fn current_frame_len(&self) -> Option<usize> {
-        self.input.current_frame_len()
+        None
     }
 
     #[inline]
@@ -107,7 +88,7 @@ impl Source for BufferedSynthWaveform {
 
     #[inline]
     fn sample_rate(&self) -> u32 {
-        self.input.sample_rate()
+        self.sample_rate.0
     }
 
     #[inline]
@@ -117,18 +98,22 @@ impl Source for BufferedSynthWaveform {
 
     #[inline]
     fn try_seek(&mut self, duration: Duration) -> Result<(), SeekError> {
-        self.input.try_seek(duration)
+        self.i = (self.sample_rate.0 as f32 * duration.as_secs_f32()) as u64;
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::source::synth_waveforms::*;
+    use crate::source::synth::*;
 
     #[test]
     fn square() {
-        let mut wf =
-            BufferedSynthWaveform::new(cpal::SampleRate(1000), 250, SynthWaveformFunction::Square);
+        let mut wf = SynthWaveform::new(
+            cpal::SampleRate(2000),
+            500.0f32,
+            SynthWaveformFunction::Square,
+        );
         assert_eq!(wf.next(), Some(1.0f32));
         assert_eq!(wf.next(), Some(1.0f32));
         assert_eq!(wf.next(), Some(-1.0f32));
@@ -141,9 +126,9 @@ mod tests {
 
     #[test]
     fn triangle() {
-        let mut wf = BufferedSynthWaveform::new(
+        let mut wf = SynthWaveform::new(
             cpal::SampleRate(8000),
-            1000,
+            1000.0f32,
             SynthWaveformFunction::Triangle,
         );
         assert_eq!(wf.next(), Some(-1.0f32));
@@ -166,8 +151,11 @@ mod tests {
 
     #[test]
     fn saw() {
-        let mut wf =
-            BufferedSynthWaveform::new(cpal::SampleRate(200), 50, SynthWaveformFunction::Sawtooth);
+        let mut wf = SynthWaveform::new(
+            cpal::SampleRate(200),
+            50.0f32,
+            SynthWaveformFunction::Sawtooth,
+        );
         assert_eq!(wf.next(), Some(0.0f32));
         assert_eq!(wf.next(), Some(0.5f32));
         assert_eq!(wf.next(), Some(-1.0f32));
@@ -176,4 +164,11 @@ mod tests {
         assert_eq!(wf.next(), Some(0.5f32));
         assert_eq!(wf.next(), Some(-1.0f32));
     }
+
+    // #[test]
+    // fn sine() {
+    //     let mut wf =
+    //         SynthWaveform::new(cpal::SampleRate(1000), 100f32, SynthWaveformFunction::Sine);
+    //
+    // }
 }

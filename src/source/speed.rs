0144699ad7157df3e54f28a69e826cd62b775e61
ugochukwu-850 <ugochukwchizaramoku@gmail.com>
2024-10-04 21:05:09 +0100

    Added documentation for Issues #566 Documented how playback speed mutation works and the effects with examples

--- src/source/speed.rs
+++ src/source/speed.rs
@@ -1,3 +1,50 @@
+//! Playback Speed control Module.
+//!
+//! The main concept of this module is the [`Speed`] struct, which
+//! encapsulates playback speed controls of the current sink. 
+//! 
+//! In order to speed up a sink, the speed struct:
+//! - Increases the current sample rate by the given factor 
+//! - Updates the total duration function to cover for the new factor by dividing by the factor
+//! - Update the pos function by multiplying the position by the factor
+//!
+//! To speed up a source from sink all you need to do is call the   `set_speed(factor: f32)` function 
+//! For example, here is how you speed up your sound by using sink or playing raw
+//!
+//! ```no_run
+//! use std::fs::File;
+//! use std::io::BufReader;
+//! use rodio::{Decoder, Sink, OutputStream, source::{Source, SineWave}};
+//!
+//! // Get an output stream handle to the default physical sound device.
+//! // Note that no sound will be played if _stream is dropped
+//! let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+//! // Load a sound from a file, using a path relative to Cargo.toml
+//! let file = BufReader::new(File::open("examples/music.ogg").unwrap());
+//! // Decode that sound file into a source
+//! let source = Decoder::new(file).unwrap();
+//! // Play the sound directly on the device 2x faster
+//! stream_handle.play_raw(source.convert_samples().speed(2.0));
+//!
+//! // The sound plays in a separate audio thread,
+//! // so we need to keep the main thread alive while it's playing.
+//! std::thread::sleep(std::time::Duration::from_secs(5));
+//! 
+//! // here is how you would do it using the sink
+//! 
+
+//! let source = SineWave::new(440.0)
+//! .take_duration(Duration::from_secs_f32(20.25))
+//! .amplify(0.20);
+//! 
+//! //! let sink = Sink::try_new(&stream_handle)?;
+//! sink.set_speed(2.0);
+//! sink.append(source);
+//! std::thread::sleep(std::time::Duration::from_secs(5));
+//! ```
+//! Notice the increase in pitch as the factor increases
+//! This is due to the higher audio data stream increasing the frequency 
+
 use std::time::Duration;
 
 use crate::{Sample, Source};
@@ -98,11 +145,6 @@ where
 
     #[inline]
     fn try_seek(&mut self, pos: Duration) -> Result<(), SeekError> {
-        /* TODO: This might be wrong, I do not know how speed achieves its speedup
-         * so I can not reason about the correctness.
-         * <dvdsk noreply@davidsk.dev> */
-
-        // even after 24 hours of playback f32 has enough precision
         let pos_accounting_for_speedup = pos.mul_f32(self.factor);
         self.input.try_seek(pos_accounting_for_speedup)
     }

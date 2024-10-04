20daf244dbebed120ad5a925b4dcc8302c1c1505
ugochukwu-850 <ugochukwchizaramoku@gmail.com>
2024-10-04 20:52:28 +0100

    Added Note on the set_speed function of impact on pitch

--- src/sink.rs
+++ src/sink.rs
@@ -193,6 +193,8 @@ impl Sink {
     ///
     /// The value `1.0` is the "normal" speed (unfiltered input). Any value other than `1.0` will
     /// change the play speed of the sound.
+    /// 
+    /// **Note that changing the speed would affect the pitch accordingly**
     #[inline]
     pub fn set_speed(&self, value: f32) {
         *self.controls.speed.lock().unwrap() = value;

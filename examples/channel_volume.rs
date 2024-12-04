75f9f920720d3d96737d1347e6d729bddf8833c5
Ando <divinegod@gmail.com>
2024-12-04 14:49:48 +1100

    fix channel_volume example to new api

--- examples/channel_volume.rs
+++ examples/channel_volume.rs
@@ -1,11 +1,11 @@
 use rodio::source::ChannelVolume;
 
 fn main() {
-    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = rodio::Sink::try_new(&handle).unwrap();
+    let stream_handle = rodio::OutputStreamBuilder::open_default_stream().unwrap();
+    let sink = rodio::Sink::connect_new(&stream_handle.mixer());
 
     let input = rodio::source::SineWave::new(440.0);
-    let chan_vol = ChannelVolume::new(input, vec![0.01, 0.01, 0.0, 0.0, 0.0, 0.0]);
+    let chan_vol = ChannelVolume::new(input, vec![0.01, 0.0, 0.1, 0.1, 0.1, 0.5]);
     sink.append(chan_vol);
 
     sink.sleep_until_end();

59a7fb93c076cba927caf81cc3b851731c5160a3
Jamie Hardt <jamiehardt@me.com>
2024-12-06 10:12:54 -0800

    Extracted GeneratorFunction type
    Also elaborated documentation, changed some param names.

--- src/source/signal_generator.rs
+++ src/source/signal_generator.rs
@@ -17,6 +17,24 @@ use std::time::Duration;
 use super::SeekError;
 use crate::Source;
 
+/// Generator function.
+///
+/// A generator function is the core of a signal generator, the `SignalGenerator` type uses these
+/// function to create periodic waveforms.
+///
+/// # Arguments
+///  *  An `f32` representing a time in the signal to generate. The scale of this variable is
+///  normalized to the period of the signal, such that "0.0" is time zero, "1.0" is one period of
+///  the signal, "2.0" is two periods and so on. This function should be written to accept any
+///  float in the range (f32::MIN, f32::MAX) but `SignalGenerator` will only pass values in
+///  (0.0, 1.0) to mitigate floating point error.
+///
+/// # Returns
+///
+/// An `f32` representing the signal level at the passed time. This value should be normalized
+/// in the range [-1.0,1.0].
+type GeneratorFunction = fn(f32) -> f32;
+
 /// Waveform functions.
 #[derive(Clone, Debug)]
 pub enum Function {
@@ -30,60 +48,75 @@ pub enum Function {
     Sawtooth,
 }
 
-fn sine_signal(theta: f32) -> f32 {
-    (TAU * theta).sin()
+fn sine_signal(phase: f32) -> f32 {
+    (TAU * phase).sin()
 }
 
-fn triangle_signal(theta: f32) -> f32 {
-    4.0f32 * (theta - (theta + 0.5f32).floor()).abs() - 1f32
+fn triangle_signal(phase: f32) -> f32 {
+    4.0f32 * (phase - (phase + 0.5f32).floor()).abs() - 1f32
 }
 
-fn square_signal(theta: f32) -> f32 {
-    if theta % 1.0f32 < 0.5f32 {
+fn square_signal(phase: f32) -> f32 {
+    if phase % 1.0f32 < 0.5f32 {
         1.0f32
     } else {
         -1.0f32
     }
 }
 
-fn sawtooth_signal(theta: f32) -> f32 {
-    2.0f32 * (theta - (theta + 0.5f32).floor())
+fn sawtooth_signal(phase: f32) -> f32 {
+    2.0f32 * (phase - (phase + 0.5f32).floor())
 }
 
 /// An infinite source that produces one of a selection of test waveforms.
 #[derive(Clone, Debug)]
 pub struct SignalGenerator {
     sample_rate: cpal::SampleRate,
-    function: fn(f32) -> f32,
+    function: GeneratorFunction,
     phase_step: f32,
     phase: f32,
     period: f32,
 }
 
 impl SignalGenerator {
-    /// Create a new `TestWaveform` object that generates an endless waveform
+    /// Create a new `SignalGenerator` object that generates an endless waveform
     /// `f`.
     ///
     /// # Panics
     ///
     /// Will panic if `frequency` is equal to zero.
     #[inline]
-    pub fn new(sample_rate: cpal::SampleRate, frequency: f32, f: Function) -> SignalGenerator {
-        assert!(frequency != 0.0, "frequency must be greater than zero");
-        let period = sample_rate.0 as f32 / frequency;
-        let delta_theta = 1.0f32 / period;
-
-        let function: fn(f32) -> f32 = match f {
+    pub fn new(sample_rate: cpal::SampleRate, frequency: f32, f: Function) -> Self {
+        let function: GeneratorFunction = match f {
             Function::Sine => sine_signal,
             Function::Triangle => triangle_signal,
             Function::Square => square_signal,
             Function::Sawtooth => sawtooth_signal,
         };
 
+        Self::with_function(sample_rate, frequency, function)
+    }
+
+    /// Create a new `SignalGenerator` object that generates an endless waveform
+    /// from the [generator function](GeneratorFunction) `generator_function`.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if `frequency` is equal to zero.
+    #[inline]
+    pub fn with_function(
+        sample_rate: cpal::SampleRate,
+        frequency: f32,
+        generator_function: GeneratorFunction,
+    ) -> Self {
+        assert!(frequency != 0.0, "frequency must be greater than zero");
+        let period = sample_rate.0 as f32 / frequency;
+        let phase_step = 1.0f32 / period;
+
         SignalGenerator {
             sample_rate,
-            function,
-            phase_step: delta_theta,
+            function: generator_function,
+            phase_step,
             phase: 0.0f32,
             period,
         }

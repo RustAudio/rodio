67b16a1368c10fc885737113798504afdc49883a
Jamie Hardt <jamiehardt@me.com>
2024-12-08 11:44:17 -0800

    Typos and added methods, also documentation

--- src/source/channel_router.rs
+++ src/source/channel_router.rs
@@ -56,17 +56,29 @@ where
         Self {
             input,
             channel_map,
-            current_channel: channel_count, // this will cause the input buffer to fill on first
-            // call to next()
+            current_channel: channel_count,
+            // this will cause the input buffer to fill on first call to next()
             channel_count,
             input_buffer: vec![],
         }
     }
 
-    /// Add or update the gain setting for a channel mapping.
-    pub fn map(&mut self, from: u16, to: u16, gain: f32) -> () {
+    /// Set or update the gain setting for a channel mapping.
+    ///
+    /// A channel from the input may be routed to any number of channels in the output, and a
+    /// channel in the output may be a mix of any number of channels in the input.
+    ///
+    /// Successive calls to `route` with the same `from` and `to` arguments will replace the
+    /// previous gain value with the new one.
+    pub fn route(&mut self, from: u16, to: u16, gain: f32) -> () {
         let k = InputOutputPair(from, to);
-        self.channel_map.insert(k, gain);
+        _ = self.channel_map.insert(k, gain);
+    }
+
+    /// Delete an existing mapping from `from` to `to` if it exists.
+    pub fn unroute(&mut self, from: u16, to: u16) -> () {
+        let k = InputOutputPair(from, to);
+        _ = self.channel_map.remove(&k);
     }
 
     /// Destroys this router and returns the underlying source.
@@ -75,7 +87,7 @@ where
         self.input
     }
 
-    /// Get mutable access to the iterator
+    /// Get mutable access to the inner source.
     #[inline]
     pub fn inner_mut(&mut self) -> &mut I {
         &mut self.input
@@ -113,22 +125,33 @@ where
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.current_channel >= self.channel_count {
+            // We've reached the end of the frame, time to grab another one from the input
             let input_channels = self.input.channels() as usize;
+
+            // This might be too fussy, a source should never break a frame in the middle of an
+            // audio frame.
             let samples_to_take = min(
                 input_channels,
                 self.input.current_frame_len().unwrap_or(usize::MAX),
             );
 
-            self.input_buffer = self.input.by_ref().take(samples_to_take).collect();
+            // fill the input buffer. If the input is exhausted and returning None this will make
+            // the input buffer zero length
+            self.input_buffer = self.inner_mut().take(samples_to_take).collect();
 
             self.current_channel = 0;
         }
 
+        // Find the output sample for current_channel
         let retval = self
             .input_buffer
             .iter()
+            // if input_buffer is empty, retval will be None
             .enumerate()
             .map(|(input_channel, in_sample)| {
+                // the way this works, the input_buffer need not be totally full, the router will
+                // work with whatever samples are available and the missing samples will be assumed
+                // to be equilibrium.
                 let pair = InputOutputPair(input_channel as u16, self.current_channel);
                 let gain = self.channel_map.get(&pair).unwrap_or(&0.0f32);
                 in_sample.amplify(*gain)

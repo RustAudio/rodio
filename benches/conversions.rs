f69ec88dac8974fa587a55e4ef54cf346ca3f03c
Petr Gladkikh <petrglad@gmail.com>
2025-02-22 21:47:24 +0400

    Keep sample conversion bench
    This may estimate additional load from onversions,
    althoug it is now less useful since f32 samples are used almost
    everythere.

--- benches/conversions.rs
+++ benches/conversions.rs
@@ -1,6 +1,6 @@
-use dasp_sample::FromSample;
+use dasp_sample::{Duplex, Sample};
 use divan::Bencher;
-use rodio::{decoder::DecoderSample, Source};
+use rodio::conversions::SampleTypeConverter;
 
 mod shared;
 
@@ -8,9 +8,16 @@ fn main() {
     divan::main();
 }
 
-// #[divan::bench(types = [i16, u16, f32])]
-// fn from_sample_to(bencher: Bencher) {
-//     bencher
-//         .with_inputs(|| shared::music_wav())
-//         .bench_values(|source| source.for_each(divan::black_box_drop))
-// }
+#[divan::bench(types = [i16, u16, f32])]
+fn from_sample<S: Duplex<f32>>(bencher: Bencher) {
+    bencher
+        .with_inputs(|| {
+            shared::music_wav()
+                .map(|s| s.to_sample::<S>())
+                .collect::<Vec<_>>()
+                .into_iter()
+        })
+        .bench_values(|source| {
+            SampleTypeConverter::<_, rodio::Sample>::new(source).for_each(divan::black_box_drop)
+        })
+}

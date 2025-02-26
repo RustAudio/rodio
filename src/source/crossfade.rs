92eb10ad0c73b3c97b7ecd0fd40dc009be69a344
dvdsk <noreply@davidsk.dev>
2025-02-26 18:55:40 +0100

    move crossfade tests to integration tests

--- src/source/crossfade.rs
+++ src/source/crossfade.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 ///
 /// Only the crossfaded portion (beginning of fadeout, beginning of fadein) is
 /// returned.
-pub fn crossfade<I1, I2>(
+pub(crate) fn crossfade<I1, I2>(
     input_fadeout: I1,
     input_fadein: I2,
     duration: Duration,
@@ -28,49 +28,3 @@ where
 /// Only the crossfaded portion (beginning of fadeout, beginning of fadein) is
 /// covered.
 pub type Crossfade<I1, I2> = Mix<TakeDuration<I1>, FadeIn<TakeDuration<I2>>>;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::buffer::SamplesBuffer;
-    use crate::source::Zero;
-
-    fn dummy_source(length: u8) -> SamplesBuffer {
-        let data: Vec<f32> = (1..=length).map(f32::from).collect();
-        SamplesBuffer::new(1, 1, data)
-    }
-
-    #[test]
-    fn test_crossfade_with_self() {
-        let source1 = dummy_source(10);
-        let source2 = dummy_source(10);
-        let mut mixed = crossfade(
-            source1,
-            source2,
-            Duration::from_secs(5) + Duration::from_nanos(1),
-        );
-        assert_eq!(mixed.next(), Some(1.0));
-        assert_eq!(mixed.next(), Some(2.0));
-        assert_eq!(mixed.next(), Some(3.0));
-        assert_eq!(mixed.next(), Some(4.0));
-        assert_eq!(mixed.next(), Some(5.0));
-        assert_eq!(mixed.next(), None);
-    }
-
-    #[test]
-    fn test_crossfade() {
-        let source1 = dummy_source(10);
-        let source2 = Zero::new(1, 1);
-        let mixed = crossfade(
-            source1,
-            source2,
-            Duration::from_secs(5) + Duration::from_nanos(1),
-        );
-        let result = mixed.collect::<Vec<_>>();
-        assert_eq!(result.len(), 5);
-        assert!(result
-            .iter()
-            .zip(vec![1.0, 2.0 * 0.8, 3.0 * 0.6, 4.0 * 0.4, 5.0 * 0.2])
-            .all(|(a, b)| (a - b).abs() < 1e-6));
-    }
-}

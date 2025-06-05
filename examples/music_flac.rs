3ce903fad86a4a9dfc3b77c412a4ff4134eccaa3
dvdsk <noreply@davidsk.dev>
2025-06-05 12:20:36 +0200

    Make accidentally dropping OutputStream impossible
    This adds a drop bomb to OutputStream. Dropping a OutputStream will panic
    (unless we are dropping as part of unwinding). Technically OutputStream
    is now a run time enforced linear type.
    
    To get rid of a `OutputStream` you now have to call `OutputStream::close`.

--- examples/music_flac.rs
+++ examples/music_flac.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()?;
     let sink = rodio::Sink::connect_new(stream_handle.mixer());
+    drop(stream_handle);
 
     let file = std::fs::File::open("assets/music.flac")?;
     sink.append(rodio::Decoder::try_from(file)?);

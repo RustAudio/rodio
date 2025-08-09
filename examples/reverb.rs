d9dd3a6f6d411c00449c1e417ec9a0ce50cf3fe9
dvdsk <noreply@davidsk.dev>
2025-08-09 20:33:34 +0200

    Fix examples not compiling due to mixer Arc
    No idea why this broke though.... I did not touch it during rebase.

--- examples/reverb.rs
+++ examples/reverb.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()?;
-    let sink = rodio::Sink::connect_new(stream_handle.mixer());
+    let sink = rodio::Sink::connect_new(&stream_handle.mixer());
 
     let file = std::fs::File::open("assets/music.ogg")?;
     let source = rodio::Decoder::try_from(file)?;

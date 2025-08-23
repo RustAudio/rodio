44adb275ca4393d71419a07dde1af3978e6f8605
dvdsk <noreply@davidsk.dev>
2025-08-23 14:15:03 +0200

    fix tests

--- examples/microphone.rs
+++ examples/microphone.rs
@@ -2,7 +2,6 @@ use inquire::Select;
 use rodio::microphone::{self, MicrophoneBuilder};
 use rodio::Source;
 use std::error::Error;
-use std::num::NonZero;
 use std::thread;
 use std::time::Duration;
 
@@ -19,7 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .open_stream()?;
 
     println!("Recording 5 seconds of input to play back");
-    let mut recording = input.take_duration(Duration::from_secs(5)).record();
+    let recording = input.take_duration(Duration::from_secs(5)).record();
 
     println!("Playing the recording");
     let mut output = rodio::OutputStreamBuilder::open_default_stream()?;

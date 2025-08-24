9ac2ea41da2af17e99d1b18b02a2c309423b7ae5
David Campbell <dcampbell24@gmail.com>
2025-08-24 13:17:57 -0400

    Show an example of compliling the sound into the binary.

--- examples/music_ogg.rs
+++ examples/music_ogg.rs
@@ -1,11 +1,12 @@
-use std::error::Error;
+use std::{error::Error, io::Cursor};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()?;
     let sink = rodio::Sink::connect_new(stream_handle.mixer());
 
-    let file = std::fs::File::open("assets/music.ogg")?;
-    sink.append(rodio::Decoder::try_from(file)?);
+    let file = include_bytes!("../assets/music.ogg");
+    let cursor = Cursor::new(file);
+    sink.append(rodio::Decoder::try_from(cursor)?);
 
     sink.sleep_until_end();
 
